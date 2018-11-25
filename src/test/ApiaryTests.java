fe4e17e1d672c1916a2869b21d339ec511c20018
SJTreadway <treadway08@gmail.com>
2018-11-25 16:55:51 -0500

    adds test for Apiary Singleton class.

--- src/test/ApiaryTests.java
+++ src/test/ApiaryTests.java
@@ -1,12 +1,18 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import singleton.Apiary;
 
 public class ApiaryTests {
     
     @Test
-    public void test() {
-        
+    public void testApiarySingleton() {
+        Apiary apiary = Apiary.getInstance();
+        Apiary apiary2 = Apiary.getInstance();
+        assertTrue(apiary == apiary2);
     }
     
 }
