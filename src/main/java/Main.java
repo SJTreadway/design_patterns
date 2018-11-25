8956051e728f17565c631ef3704306d05727227f
SJTreadway <treadway08@gmail.com>
2018-11-25 16:29:41 -0500

    creates functionality in main to showcase the Strategy design pattern.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,33 +18,43 @@ public class Main {
         
         // SINGLETON Design Pattern
         System.out.println("---------------Singleton design pattern---------------");
+        
         Apiary apiary = Apiary.getInstance();
         System.out.println("apiary1 = Apiary.getInstance()");
+        
         Apiary apiary2 = Apiary.getInstance();
         System.out.println("apiary2 = Apiary.getInstance()");
+        
         System.out.print("Are these two apiary vars equal? ");
         System.out.print(apiary == apiary2);
         System.out.println("\n------------------------------------------------------\n\n");
         
         // BUILDER Design Pattern
         System.out.println("---------------Builder design pattern-----------------");
+        
         Room room = new Room("Main Room", 50, 60);
         BeehiveBuilder bhb = new BeehiveBuilder(room);
         System.out.println("BeehiveBuilder created, containing " + room.getName() 
                 + " with these dimensions: " + room.getLength() + "x" + room.getWidth());
+        
         Beehive bh = bhb.build();
         System.out.println("How many rooms does our beehive contain? " + bh.numRooms());
+        
         Room room2 = new Room("Game Room", 25, 20);
         System.out.println("Game Room created with these dimensions: " 
                 + room2.getLength() + "x" + room2.getWidth());
+        
         System.out.println("Adding Game Room to our Beehive...");
         bh.addRoom(room2);
-        System.out.println("How many rooms does our beehive contain? " + bh.numRooms());
+        
+        System.out.println("How many rooms does our beehive contain? " + bh.numRooms());       
         System.out.println("------------------------------------------------------\n\n");
         
         // DECORATOR Design Pattern
         System.out.println("---------------Decorator design pattern---------------");
+        
         System.out.println("Creating a HoneyBee, BumbleBee, CarpenterBee, and SweatBee...");
+        
         HoneyBee honeybee = new HoneyBee("Honey Bee 1", "male", "harvester", 3);
         BumbleBee bumblebee = new BumbleBee("Bumble Bee 1", "male", "fighter", 5);
         CarpenterBee carpenterbee = new CarpenterBee("Carpenter Bee 1", "female", "worker", 2);
@@ -55,17 +65,16 @@ public class Main {
         bumblebee.specialAbility();
         carpenterbee.specialAbility();
         sweatbee.specialAbility();
-        
-        System.out.print("\nList of actions for each type of Bee: \n");
-        honeybee.performAction();
-        bumblebee.performAction();
-        carpenterbee.performAction();
-        sweatbee.performAction();
         System.out.println("------------------------------------------------------\n\n");
         
         // STRATEGY Design Pattern
         System.out.println("---------------Strategy design pattern----------------");
         
+        System.out.print("List of actions for each type of Bee: \n");
+        honeybee.performAction();
+        bumblebee.performAction();
+        carpenterbee.performAction();
+        sweatbee.performAction();
         System.out.println("------------------------------------------------------");
         
     }
