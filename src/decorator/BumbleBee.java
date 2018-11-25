108a6515dd6630bf58c45be775e9f40cf1ec0741
SJTreadway <treadway08@gmail.com>
2018-11-25 15:17:57 -0500

    forgot to include BumbleBee in last commit, which implements our BeeAction interface.

--- src/decorator/BumbleBee.java
+++ src/decorator/BumbleBee.java
@@ -3,7 +3,9 @@ package decorator;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BumbleBee extends GenericBee implements Bee {
+import strategy.BeeAction;
+
+public class BumbleBee extends GenericBee implements Bee, BeeAction {
     private static final Map<String, Integer> attributes;
     static {
         attributes = new HashMap<String, Integer>();
@@ -19,5 +21,9 @@ public class BumbleBee extends GenericBee implements Bee {
 
     public void specialAbility() {
         System.out.println("Increased fighting attribute!");
+    }
+    
+    public void performAction() {
+        System.out.println("You have decided to fight!");
     }
 }
