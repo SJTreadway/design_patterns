3f598d88ee5ed5664a614844a738e41cc9bdf072
SJTreadway <treadway08@gmail.com>
2018-11-24 21:13:37 -0500

    adds checkstyle xml config file and sets up project in eclipse.

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Main class that runs our program.
+ */
+
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+    }
+}
